32742e3ff5d2b2a9534f153665d239c6da6e123c
Mathew <mattcohen526@gmail.com>
2023-03-20 23:32:32 -0400

    res menu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 
 #include <iostream>
 
+
 int main()
 {
     std::cout << "I like men!\n";
+
+    std::cout << "Which res are you using? \n(1) 1080\n(2) 1440\nInput -> ";
+    int input{};
+    std::cin >> input;
+
+
+
+    
 }
