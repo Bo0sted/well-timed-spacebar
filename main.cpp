ccfe6f475610037cfe7f8674d46795e6863c03de
12problems <74747315+12problems@users.noreply.github.com>
2023-03-20 23:29:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 //
 
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
     std::cout << "I like men!\n";
+
+
+    //1920x1080
+    //Vector3 CIRCLE_OFFSET = new Vec3(895, 473, 0);
+    //CIRCLE_TREMBLE_RANGE = 50;
+    //CIRCLE_DIAMETER = 133;
+    //SKILL_CHECK_RECT = new Rectangle(234, 140, 1500, 700);
+
+    //1440p
+    //Fuq you you can get your own values
 }
