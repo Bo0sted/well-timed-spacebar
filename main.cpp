95bf0a12f41a0186a0612224e49be447f983f7c6
12problems <74747315+12problems@users.noreply.github.com>
2023-03-26 01:30:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,18 +13,37 @@ cv::Mat getMat(HWND hWND) {
     HDC deviceContext = GetDC(hWND);
     HDC memoryDeviceContext = CreateCompatibleDC(deviceContext);
 
-    RECT windowRect; //Window rectangle, to be changed later according to resolution because we don't need to capture the whole screen (save processing time)
-    GetClientRect(hWND, &windowRect);
+    int height, width;
+    HBITMAP bitmap;
+    if (true) { //DBD window
+        bool nineteentwenty = true; //Current resolution workaround without having to do a bunch more stuff
+        if (nineteentwenty) {
+            height = 285;
+            width = 253;
+            bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+            SelectObject(memoryDeviceContext, bitmap);
+            //Copy data into bitmap
+            BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 846, 405, SRCCOPY);
+        }
+        else {
+            height = 300;
+            width = 350;
+            bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+            SelectObject(memoryDeviceContext, bitmap);
+            BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 1194, 635, SRCCOPY);
+        }
+    }
+    else { //Other debug window
+        RECT windowRect; //Window rectangle, to be changed later according to resolution because we don't need to capture the whole screen (save processing time)
+        GetClientRect(hWND, &windowRect);
+        height = windowRect.bottom;
+        width = windowRect.right;
 
-    int height = windowRect.bottom;
-    int width = windowRect.right;
-
-    HBITMAP bitmap = CreateCompatibleBitmap(deviceContext, width, height);
-
-    SelectObject(memoryDeviceContext, bitmap);
-
-    //Copy data into bitmap
-    BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
+        bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+        SelectObject(memoryDeviceContext, bitmap);
+        //Copy data into bitmap
+        BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
+    }
 
     BITMAPINFOHEADER bi;
     bi.biSize = sizeof(BITMAPINFOHEADER);
@@ -52,9 +71,10 @@ cv::Mat getMat(HWND hWND) {
 
 int main()
 {
-    //LPCWSTR windowTitle = L"DeadByDaylight  ";
-    LPCWSTR windowTitle = L"WindowTitle";
+    LPCWSTR windowTitle = L"DeadByDaylight  ";
+    //LPCWSTR windowTitle = L"Window title";
     HWND hWND = FindWindow(NULL, windowTitle);
+
     bool init = false;
     while (!hWND) { //If window not found
         if (!init) {
@@ -67,38 +87,36 @@ int main()
     }
 
     std::cout << "\nFound window\n";
-    RECT windowRect; //Window rectangle, to be changed later according to resolution because we don't need to capture the whole screen (save processing time)
-    GetClientRect(hWND, &windowRect);
-    int height = windowRect.bottom;
-    int width = windowRect.right;
-    cv::Mat background/* = cv::Mat(height, width, CV_8UC4) */ ;
+
+    cv::Mat background;
     cv::namedWindow("output", cv::WINDOW_NORMAL); //Create the output window, debug purposes.
 
-    std::string filePath = cv::samples::findFile("findMe.jpg"); //"Needle" to find in the window
+    std::string filePath = cv::samples::findFile("spacebar.png"); //"Needle" to find in the window
     cv::Mat needle = cv::imread(filePath, cv::IMREAD_UNCHANGED);
-    cv::cvtColor(needle, needle, cv::COLOR_BGR2GRAY); //marchTemplate requires grayscale so we convert to gray
+    cv::cvtColor(needle, needle, cv::COLOR_BGR2RGBA); //marchTemplate requires grayscale so we convert to gray
+    cv::Mat img, copyofimg, result; //Make a copy to keep for later
+    cv::Point minLoc, /*Minimum location*/ maxLoc; //Max location, which we will use as basis for "is this the skill check?"
+    double minVal, maxVal; //Min and max values
     while (true) {
-        cv::Mat img = getMat(hWND); //Get the window
-        cv::Mat copyofimg; //Make a copy to keep for later
-        img.copyTo(copyofimg);
-        cv::cvtColor(copyofimg, copyofimg, cv::COLOR_BGR2GRAY); //marchTemplate requires grayscale so we convert to gray
+        img = getMat(hWND); //Get the window
 
-        cv::Mat result; //Result mat to store the result of matchTemplate
+        img.copyTo(copyofimg);
+        cv::cvtColor(copyofimg, copyofimg, cv::COLOR_BGR2RGBA); //marchTemplate requires grayscale so we convert to gray
+
         cv::matchTemplate(copyofimg, needle, result, cv::TM_CCOEFF_NORMED, cv::Mat());
         //cv::normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat()); //Normalize the data, might need to use?
         
-        cv::Point minLoc; //Minimum location
-        cv::Point maxLoc; //Max location, which we will use as basis for "is this the skill check?"
-        double minVal, maxVal; //Min and max values
         cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat()); //Find the min and max values of the result from matchTemplate 
+        std::cout << minVal << " " << maxVal << "\n"; //DEBUG PURPOSES: To see the minimum value and maximum value. Used to fine tune the threshold
         if (maxVal >= THRESHOLD) { //"Found"
-            cv::rectangle(img, maxLoc, cv::Point(maxLoc.x + needle.cols, maxLoc.y + needle.rows), cv::Scalar(255, 0, 0), 2, 8, 0); //We draw a rectangle around the found image on original "img"
+            std::cout << "Found\n";
+            cv::rectangle(img, maxLoc, cv::Point(maxLoc.x + needle.cols, maxLoc.y + needle.rows), cv::Scalar(0, 255, 0), 2, 8, 0); //We draw a rectangle around the found image on original "img"
         }
-        //std::cout << minVal << " " << maxVal << "\n"; //DEBUG PURPOSES: To see the minimum value and maximum value. Used to fine tune the threshold
-        img.copyTo(background); //Copy to background
 
+        img.copyTo(background); //Copy to background
         cv::imshow("output", background); //Show the output
         cv::waitKey(33); //33ms means 30fps. 50ms would be 20fps. 100ms would be 10fps. 10ms would be 100fps
+        //Note: Most likely limited by computing power, not this.
     }
 
     //You must either CTRL+C the console window or close the console window.
@@ -107,7 +125,6 @@ int main()
     //Vector3 CIRCLE_OFFSET = new Vec3(895, 473, 0);
     //CIRCLE_TREMBLE_RANGE = 50;
     //CIRCLE_DIAMETER = 133;
-    //SKILL_CHECK_RECT = new Rectangle(234, 140, 1500, 700);
 
     //1440p
 
